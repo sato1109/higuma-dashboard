4ac5bf389c61b8891ae5d63815badf030f5e602f
sato1109 <b1021113@fun.ac.jp>
2024-06-30 15:37:41 +0900

    カラーバーの表示

--- src/higuma_dashboard/streamlit.py
+++ src/higuma_dashboard/streamlit.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import folium
 from streamlit_folium import folium_static
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 # Streamlitのタイトル
 st.title("Folium Map in Streamlit - Hakodate")
@@ -36,5 +39,30 @@ folium.Marker(
     icon=custom_icon
 ).add_to(m)
 
-# Streamlit上に地図を表示
-folium_static(m)
+# カラーバーを作成する関数
+def create_color_bar():
+    # 色の範囲を定義
+    colors = ["#ffa07a", "#ff6347", "#ff0000"]
+    labels = ["1 - 5", "6 - 10", "11 -"]
+
+    # カラーバーを作成
+    fig, ax = plt.subplots(figsize=(0.1, 0.5))  # サイズを小さくする
+    cmap = plt.cm.colors.ListedColormap(colors)  # カラーマップをリストから作成
+    norm = plt.Normalize(vmin=0, vmax=len(colors))
+    cb = plt.colorbar(plt.cm.ScalarMappable(norm=norm, cmap=cmap), cax=ax)
+    cb.set_ticks(np.arange(len(colors)) + 0.5)
+    cb.set_ticklabels(labels)
+    cb.ax.invert_yaxis()
+    cb.ax.tick_params(labelsize=5)  # フォントサイズを小さくする
+
+    return fig
+
+# レイアウトを調整
+col1, col2 = st.columns([6, 1])  # カラムの比率を調整
+
+with col1:
+    folium_static(m)
+with col2:
+    fig = create_color_bar()
+    st.pyplot(fig)
+
